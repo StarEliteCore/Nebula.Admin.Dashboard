c77ec6c9c3cd831c91b243516a5164943b4a1af9
大黄瓜 <179722134@qq.com>
2020-10-16 23:46:23 +0800

    提交代码

--- destiny-admin-flow-vue/src/views/system/code-generator-managerment/code-generator.ts
+++ destiny-admin-flow-vue/src/views/system/code-generator-managerment/code-generator.ts
@@ -1,6 +1,8 @@
 import { Component, Emit, Mixins, Ref } from "vue-property-decorator";
 import { EFilterConnect, EFilterOprator } from '@/shared/request/query.enum';
 
+import { IPropertyMetadataDto } from '@/domain/entity/codeGenerator/PropertyMetadataDto';
+import { ITableColumn } from '@/shared/table/ITable';
 import PageMixins from '@/shared/mixins/page.mixins';
 
 @Component({
@@ -8,6 +10,29 @@ import PageMixins from '@/shared/mixins/page.mixins';
 })
 
 export default class CodeGeneratorManagerment extends Mixins(PageMixins) {
-    
+    private default:IPropertyMetadataDto={
+        isPrimaryKey:false,
+        propertyName:"1111",
+        displayName:"",
+        isNullable:false,
+        defaultValue:"",
+        cSharpType:"",
+        isInputDto:false,
+        isOutputDto:false,
+        isPageDto:false,
+    }
 
+    private columns: ITableColumn[] = [
+
+          {
+              title: "属性名",
+              slot: "Name",
+              align: "center"
+            }
+          ];
+    private PropertyArr:IPropertyMetadataDto[]=[]
+    private created()
+    {
+        this.PropertyArr.push(this.default);
+    }
 }
